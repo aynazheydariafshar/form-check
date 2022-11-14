db6379041185e3646581570944a8964ae6346ebb
aynazheydariafshar <aynazhadriafshar@gmail.com>
2022-11-14 17:11:47 +0330

    add ui for not found

--- my-app/src/routes/NotFound.jsx
+++ my-app/src/routes/NotFound.jsx
@@ -2,10 +2,18 @@ import { Box, Typography } from "@mui/material";
 
 const NotFound = () => {
   return (
-    <Box className="w-full h-[100vh] flex justify-center items-center">
-      <Typography variant="h5" className="font-bold">
-        Not Found
-      </Typography>
+    <Box
+      style={{ height: "100vh" }}
+      className="flex-col flex bg-gradient-to-r from-orange-500 to-blue-300 justify-center items-center"
+    >
+      <Box
+        sx={{ boxShadow: 3 }}
+        className="w-[200px] h-auto p-5 m-5 rounded-lg bg-white"
+      >
+        <Typography variant="h5" className="font-bold text-center">
+          Not Found
+        </Typography>
+      </Box>
     </Box>
   );
 };
