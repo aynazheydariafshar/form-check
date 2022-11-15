3260bc8c6b6cf8ec46398fecda5be97a5ff8ebf5
aynazheydariafshar <aynazhadriafshar@gmail.com>
2022-11-15 13:14:42 +0330

    responsive cards & fixed array of skills

--- my-app/src/routes/app/ChartInfo/index.jsx
+++ my-app/src/routes/app/ChartInfo/index.jsx
@@ -1,22 +1,28 @@
+import { IconButton } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/material";
 import React from "react";
-import { useEffect } from "react";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import MyChart from "utils/components/MyChart";
+import { MY_APP } from "constant";
 
 const ChartInfo = () => {
   const user = useSelector((state) => state.info.user);
   const labels = user?.map((item) => item.age);
   const skill = user?.map((item) => item.skills.map((item) => item));
+  const navigate = useNavigate();
   const counts = [];
   const countsSkill = [];
   labels.forEach((x) => {
     counts[x] = (counts[x] || 0) + 1;
   });
   skill.forEach((x) => {
-    countsSkill[x] = (countsSkill[x] || 0) + 1;
+    x.forEach((item) => {
+      countsSkill[item] = (countsSkill[item] || 0) + 1;
+    });
   });
 
   const dataAge = {
@@ -40,22 +46,28 @@ const ChartInfo = () => {
       },
     ],
   };
+  console.log(Object.keys(countsSkill));
   return (
-    <Box
-      style={{ height: "100vh" }}
-      className="flex-col flex bg-gradient-to-r from-orange-500 to-blue-300 justify-center items-center"
-    >
+    <Box className="flex-col overflow-y-auto min-h-screen h-full flex bg-gradient-to-r from-orange-500 to-blue-300 justify-center items-center">
+      <Box className="w-full flex px-5">
+        <IconButton onClick={() => navigate(`${MY_APP}`)}>
+          <HomeRoundedIcon
+            fontSize="large"
+            className="text-black text-left hover:text-white"
+          />
+        </IconButton>
+      </Box>
       {user.length > 0 ? (
         <>
           <Box
             sx={{ boxShadow: 3 }}
-            className="w-[500px] h-auto m-5 rounded-lg bg-white"
+            className="w-[40%] h-auto m-5 rounded-lg bg-white"
           >
             <MyChart data={dataAge} />
           </Box>
           <Box
             sx={{ boxShadow: 3 }}
-            className="w-[500px] h-auto m-5 rounded-lg bg-white"
+            className="w-[40%] h-auto m-5 rounded-lg bg-white"
           >
             <MyChart data={dataSkills} />
           </Box>
