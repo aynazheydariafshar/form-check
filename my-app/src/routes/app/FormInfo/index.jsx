6c253ac376761dd26f3e89bda81f36eccc387877
aynazheydariafshar <aynazhadriafshar@gmail.com>
2022-11-15 12:35:41 +0330

    responsive cards

--- my-app/src/routes/app/FormInfo/index.jsx
+++ my-app/src/routes/app/FormInfo/index.jsx
@@ -1,25 +1,25 @@
 import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+
 import FormInput from "./components/FormInput";
 import TableInfo from "./components/TableInfo";
-import { useSelector } from "react-redux";
 
 const FormInfo = () => {
   const user = useSelector((state) => state.info.user);
 
   return (
     <Box
-      style={{ height: user.length > 3 ? "auto" : "100vh" }}
-      className="flex-col flex bg-gradient-to-r from-orange-500 to-blue-300 justify-center items-center"
+      className="flex-col overflow-y-auto min-h-screen h-full flex bg-gradient-to-r from-orange-500 to-blue-300 justify-center items-center"
     >
       <Box
         sx={{ boxShadow: 3 }}
-        className="w-[500px] h-auto m-5 rounded-lg bg-white"
+        className="w-[35%] h-auto m-5 rounded-lg bg-white"
       >
         <FormInput />
       </Box>
       <Box
         sx={{ boxShadow: 3 }}
-        className="w-[800px] h-auto m-5 rounded-lg bg-white"
+        className="w-[70%] overflow-x-auto h-auto m-5 rounded-lg bg-white"
       >
         {user.length > 0 && <TableInfo />}
       </Box>
